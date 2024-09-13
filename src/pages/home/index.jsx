72b61f43629e0835b111a5d06f4ce684c02daf1e
Alisson Strey Gross <TR051885@MinhaTI.com.br>
2024-09-13 12:13:47 -0400

    styling with react

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,78 +1,83 @@
 import './style.css';
+import { Container, Button, Grid, Typography } from '@mui/material';
 
 function Home() {
   return (
-    <div className="calculadora">
-      <p id="resultado"></p>
-      <table>
-        <tbody>
-          <tr>
-            <td colSpan="2">
-              <button className="colspan2 softGrey">{'<'}</button>
-            </td>
-            <td>
-              <button className="softGrey">c</button>
-            </td>
-            <td>
-              <button className="yellow">x</button>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <button className="grey">1</button>
-            </td>
-            <td>
-              <button className="grey">2</button>
-            </td>
-            <td>
-              <button className="grey">3</button>
-            </td>
-            <td>
-              <button className="yellow">/</button>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <button className="grey">4</button>
-            </td>
-            <td>
-              <button className="grey">5</button>
-            </td>
-            <td>
-              <button className="grey">6</button>
-            </td>
-            <td>
-              <button className="yellow">+</button>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <button className="grey">7</button>
-            </td>
-            <td>
-              <button className="grey">8</button>
-            </td>
-            <td>
-              <button className="grey">9</button>
-            </td>
-            <td>
-              <button className="yellow">-</button>
-            </td>
-          </tr>
-          <tr>
-            <td colSpan="2">
-              <button className="colspan2 grey">0</button>
-            </td>
-            <td>
-              <button className="grey">.</button>
-            </td>
-            <td>
-              <button className="yellow">=</button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <Container maxWidth="sm" disableGutters className="custom_container">
+      <Typography variant="h4" align="right" id="resultado" gutterBottom>
+        0
+      </Typography>
+
+      <Grid container spacing={2}>
+
+        {/*linha 1*/}
+        <Grid item xs={3}>
+          <Button fullWidth variant="outlined" className="softGrey">{'<'}</Button>
+        </Grid>
+        <Grid item xs={3}>
+          <Button fullWidth variant="outlined" className="softGrey">c</Button>
+        </Grid>
+        <Grid item xs={3}>
+          <Button fullWidth variant="outlined" className="yellow">+/-</Button>
+        </Grid>
+        <Grid item xs={3}>
+          <Button fullWidth variant="outlined" className="yellow">/</Button>
+        </Grid>
+        
+        {/*linha 2*/}
+        <Grid item xs={3}>
+          <Button fullWidth variant="outlined" className="grey">1</Button>
+        </Grid>
+        <Grid item xs={3}>
+          <Button fullWidth variant="outlined" className="grey">2</Button>
+        </Grid>
+        <Grid item xs={3}>
+          <Button fullWidth variant="outlined" className="grey">3</Button>
+        </Grid>
+        <Grid item xs={3}>
+          <Button fullWidth variant="outlined" className="yellow">x</Button>
+        </Grid>
+        
+        {/*linha 3*/}
+        <Grid item xs={3}>
+          <Button fullWidth variant="outlined" className="grey">4</Button>
+        </Grid>
+        <Grid item xs={3}>
+          <Button fullWidth variant="outlined" className="grey">5</Button>
+        </Grid>
+        <Grid item xs={3}>
+          <Button fullWidth variant="outlined" className="grey">6</Button>
+        </Grid>
+        <Grid item xs={3}>
+          <Button fullWidth variant="outlined" className="yellow">+</Button>
+        </Grid>
+        
+        {/*linha 4*/}
+        <Grid item xs={3}>
+          <Button fullWidth variant="outlined" className="grey">7</Button>
+        </Grid>
+        <Grid item xs={3}>
+          <Button fullWidth variant="outlined" className="grey">8</Button>
+        </Grid>
+        <Grid item xs={3}>
+          <Button fullWidth variant="outlined" className="grey">9</Button>
+        </Grid>
+        <Grid item xs={3}>
+          <Button fullWidth variant="outlined" className="yellow">-</Button>
+        </Grid>
+        
+        {/*linha 5*/}
+        <Grid item xs={6}>
+          <Button fullWidth variant="outlined" className="colspan2 grey">0</Button>
+        </Grid>
+        <Grid item xs={3}>
+          <Button fullWidth variant="outlined" className="grey">.</Button>
+        </Grid>
+        <Grid item xs={3}>
+          <Button fullWidth variant="outlined" className="yellow">=</Button>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
